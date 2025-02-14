b7d8998ff20337907766fc2f83fbb2a8c3114f62
TommiTerza <s317650@studenti.polito.it>
2025-02-14 18:27:39 +0100

    Fixing python package

--- src/verifit/run.py
+++ src/verifit/run.py
@@ -24,7 +24,7 @@ def verifit_run(no_build=False):
     # Create the VerifIt object
     verEnv = verifit.VerifItEnv(data)
     
-    print("Setting up VerifIt project...")
+    print("[cyan]Setting up VerifIt project...[\cyan]")
 
     # Check the presence of the required Makefile targets
     if not run_util._makefile_target_check() :
@@ -32,7 +32,7 @@ def verifit_run(no_build=False):
         rich.print(" - Please ensure that the Makefile contains the required targets")
         exit(1)
     else:
-        rich.print(" - Target project Makefile sanity check [bold green]done[/bold green]")
+        rich.print(" - Target project Makefile sanity check [bold green][✔][/bold green]")
 
     if not no_build:
         # Build the model
@@ -43,7 +43,7 @@ def verifit_run(no_build=False):
             rich.print(" - [bold red]ERROR: Model build failed![/bold red]")
             exit(1)
         else:
-            rich.print(" - Model build [bold green]done[/bold green]")
+            rich.print(" - Model build [bold green][✔][/bold green]")
 
     # If the target is an FPGA board, load the bitstream, then setup the serial connection and GDB
     if data['target']['type'] == "fpga":
@@ -55,7 +55,7 @@ def verifit_run(no_build=False):
             rich.print(" - Please ensure that the FPGA board is connected and powered on")
             exit(1)
         else:
-            rich.print(f" - Model load on FPGA board {data['target']['name']} [bold green]done[/bold green]")     
+            rich.print(f" - Model load on FPGA board {data['target']['name']} [bold green][✔][/bold green]")     
 
         with Status(" [cyan]Setting up serial connection...[/cyan]", spinner="dots") as status:
             serial_setup_success = verEnv.serial_begin()
@@ -65,7 +65,7 @@ def verifit_run(no_build=False):
             rich.print(" - Please ensure that the serial port is correctly configured")
             exit(1)
         else:
-            rich.print(" - Serial setup [bold green]done[/bold green]")
+            rich.print(" - Serial setup [bold green][✔][/bold green]")
 
         with Status(" [cyan]Setting up GDB...[/cyan]", spinner="dots") as status:
             gdb_setup_success = verEnv.setup_gdb()
@@ -74,10 +74,9 @@ def verifit_run(no_build=False):
             rich.print(" - [bold red]ERROR: GDB setup failed![/bold red]")
             exit(1)
         else:
-            rich.print(" - GDB setup [bold green]done[/bold green]")
+            rich.print(" - GDB setup [bold green][✔][/bold green]")
 
-    rich.print("VerifIt project setup [bold green]done[/bold green]")
-    rich.print("\nStarting verification campaign...")
+    rich.print("[cyan]\nStarting verification campaign...[\cyan]")
 
     with Progress(
         TextColumn("[bold cyan]{task.description}"),
@@ -86,20 +85,21 @@ def verifit_run(no_build=False):
         transient=True,
     ) as progress:
         # Run the verification campaign
-        task = progress.add_task("Running tests...", total=data['target']['iterations'] * len(data['tests']))
+        task = progress.add_task(" - Running tests...", total=data['target']['iterations'] * len(data['tests']))
 
         for test_iteration in range(data['target']['iterations']):
             if not verEnv.gen_datasets():
               rich.print(f" - [bold red]ERROR: Dataset generation failed![/bold red]")
+              exit(1)
 
             for test in data['tests']:
                 if not verEnv.launch_test(app_name=test['name'], iteration=test_iteration, pattern=rf"{data['target']['outputFormat']}", output_tags=data['target']['outputTags'], timeout_t=1000):
                     rich.print(f" - [bold red]ERROR: Test {test['name']} failed because of GDB timeout[/bold red]")
                     exit(1)
-                else:
-                    progress.update(task, advance=1, description=f"[cyan]{test_iteration + 1}/{data['target']['iterations']}: {test['name']}", refresh=True)
+                
+                progress.update(task, advance=1, description=f"[cyan]{test_iteration + 1}/{data['target']['iterations']}: {test['name']}", refresh=True)
 
-        rich.print("[bold green]All tests run![/bold green]")
+        rich.print(" - All tests run![bold green][✔][/bold green]")
         rich.print("VerifIt campaign completed")
 
 # If necessary, generates the necessary files for the VerifIt package: verifit_golden.py and config.ver
@@ -109,10 +109,10 @@ def verifit_setup():
         rich.print("[orange]WARNING: 'verifit_golden.py' already exists in the current directory.[\orange]")    
     else: 
         run_util._copy_package_file("templates/verifit_golden.py")
-        rich.print("Generation of 'verifit_golden.py' [bold green]done[/bold green]")
+        rich.print("Generation of 'verifit_golden.py' [bold green][✔][/bold green]")
     
     if os.path.exists(f"{current_directory}/config.ver"):
         rich.print("[orange]WARNING: 'config.ver' already exists in the current directory.[\orange]")
     else:
         run_util._copy_package_file("templates/config.ver")
-        rich.print("Generation of 'config.ver' [bold green]done[/bold green]")
+        rich.print("Generation of 'config.ver' [bold green][✔][/bold green]")
