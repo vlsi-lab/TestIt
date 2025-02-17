114fb4170fe233880d71c43ec25352bb915feff1
TommiTerza <s317650@studenti.polito.it>
2025-02-17 09:41:08 +0100

    Fixing python package

--- src/verifit/run.py
+++ src/verifit/run.py
@@ -5,7 +5,7 @@ import rich
 from . import verifit
 import os
 
-def verifit_run(no_build=False):
+def verifit_run(no_build=False, italian_mode=False):
     
     current_directory = os.getcwd()
 
@@ -24,7 +24,11 @@ def verifit_run(no_build=False):
     # Create the VerifIt object
     verEnv = verifit.VerifItEnv(data)
     
-    rich.print("[cyan]Setting up VerifIt project...[\cyan]")
+    if not italian_mode:
+      rich.print("[cyan]Setting up VerifIt project...[/cyan]")
+    else:
+      rich.print("[dark green]||[/dark green][white]||[/white][bold red]||[/bold red]")
+      rich.print("[cyan]Bringing salted water to boil...[/cyan]")
 
     # Check the presence of the required Makefile targets
     if not run_util._makefile_target_check() :
@@ -32,51 +36,85 @@ def verifit_run(no_build=False):
         rich.print(" - Please ensure that the Makefile contains the required targets")
         exit(1)
     else:
-        rich.print(" - Target project Makefile sanity check [bold green][✔][/bold green]")
+        if not italian_mode:
+            rich.print(" - Target project Makefile sanity check [bold green][OK][/bold green]")
+        else:
+            rich.print(" - Nonna's recipe [bold green][READ][/bold green]")
 
     if not no_build:
         # Build the model
-        with Status(" [cyan]Building model...[/cyan]", spinner="dots") as status:
-            build_success = verEnv.build_model()
+        if not italian_mode:
+            with Status(" [cyan]Building model...[/cyan]", spinner="dots") as status:
+                build_success = verEnv.build_model()
+        else:
+            with Status(" [cyan]Cutting onions...[/cyan]", spinner="dots") as status:
+                build_success = verEnv.build_model()
 
         if not build_success:
             rich.print(" - [bold red]ERROR: Model build failed![/bold red]")
             exit(1)
         else:
-            rich.print(" - Model build [bold green][✔][/bold green]")
+            if not italian_mode:
+                rich.print(" - Model build [bold green][OK][/bold green]")
+            else:
+                rich.print(" - Onions [bold green][CUT][/bold green]")
 
     # If the target is an FPGA board, load the bitstream, then setup the serial connection and GDB
     if data['target']['type'] == "fpga":
-        with Status(f" [cyan]Loading model on FPGA board {data['target']['name']}...[/cyan]", spinner="dots") as status:
-            load_success = verEnv.load_fpga_model()   
+        if not italian_mode:
+            with Status(f" [cyan]Loading model on FPGA board {data['target']['name']}...[/cyan]", spinner="dots") as status:
+                load_success = verEnv.load_fpga_model()   
+        else:
+            with Status(f" [cyan]Frying the onions in a pan...[/cyan]", spinner="dots") as status:
+                load_success = verEnv.load_fpga_model() 
 
         if not load_success:
             rich.print(f" - [bold red]ERROR: Model load on FPGA board {data['target']['name']} failed![/bold red]")
             rich.print(" - Please ensure that the FPGA board is connected and powered on")
             exit(1)
         else:
-            rich.print(f" - Model load on FPGA board {data['target']['name']} [bold green][✔][/bold green]")     
+            if not italian_mode:
+                rich.print(f" - Model load on FPGA board {data['target']['name']} [bold green][OK][/bold green]")
+            else:
+                rich.print(f" - Soffritto {data['target']['name']} [bold green][DONE][/bold green]")     
 
-        with Status(" [cyan]Setting up serial connection...[/cyan]", spinner="dots") as status:
-            serial_setup_success = verEnv.serial_begin()
+        if not italian_mode:
+            with Status(" [cyan]Setting up serial connection...[/cyan]", spinner="dots") as status:
+                serial_setup_success = verEnv.serial_begin()
+        else:
+            with Status(" [cyan]Opening a couple of pelati cans...[/cyan]", spinner="dots") as status:
+                serial_setup_success = verEnv.serial_begin
         
         if not serial_setup_success:
             rich.print(" - [bold red]ERROR: Serial setup failed![/bold red]")
             rich.print(" - Please ensure that the serial port is correctly configured")
             exit(1)
         else:
-            rich.print(" - Serial setup [bold green][✔][/bold green]")
+            if not italian_mode:
+                rich.print(" - Serial setup [bold green][OK][/bold green]")
+            else:
+                rich.print(" - Pelati [bold green][OPENED][/bold green]")
 
-        with Status(" [cyan]Setting up GDB...[/cyan]", spinner="dots") as status:
-            gdb_setup_success = verEnv.setup_gdb()
+        if not italian_mode:
+            with Status(" [cyan]Setting up GDB...[/cyan]", spinner="dots") as status:
+                gdb_setup_success = verEnv.setup_gdb()
+        else:
+            with Status(" [cyan]Cooking the pomodoro sauce...[/cyan]", spinner="dots") as status:
+                gdb_setup_success = verEnv.setup_gdb()
 
         if not gdb_setup_success:
             rich.print(" - [bold red]ERROR: GDB setup failed![/bold red]")
             exit(1)
         else:
-            rich.print(" - GDB setup [bold green][✔][/bold green]")
+            if not italian_mode:
+                rich.print(" - GDB setup [bold green][OK][/bold green]")
+            else:
+                rich.print(" - Pomodoro sauce [bold green][DONE][/bold green]") 
 
-    rich.print("[cyan]\nStarting verification campaign...[/cyan]")
+    if not italian_mode:
+        rich.print("[cyan]\nStarting verification campaign...[/cyan]")
+    else:
+        rich.print("[cyan]\nThrowing in the pasta...[/cyan]")
 
     with Progress(
         TextColumn("[bold cyan]{task.description}"),
@@ -86,7 +124,12 @@ def verifit_run(no_build=False):
         transient=True,
     ) as progress:
         # Run the verification campaign
-        task = progress.add_task(" - Running tests...", total=data['target']['iterations'] * len(data['tests']), start=False)
+        if not italian_mode:
+          task_message = " - Running tests..."
+        else:
+          task_message = " - Cooking the pasta..."
+
+        task = progress.add_task(task_message, total=data['target']['iterations'] * len(data['tests']), start=False)
         start = False
 
         for test_iteration in range(data['target']['iterations']):
@@ -103,9 +146,13 @@ def verifit_run(no_build=False):
                     start = True
 
                 progress.update(task, advance=1, description=f" - [cyan]{test_iteration + 1}/{data['target']['iterations']}: {test['name']}", refresh=True)
-
-        rich.print(" - All tests run![bold green][✔][/bold green]")
-        rich.print("VerifIt campaign completed")
+        
+        if not italian_mode:
+            rich.print(" - All tests run![bold green][OK][/bold green]")
+            rich.print("VerifIt campaign completed")
+        else:
+            rich.print(" - Pasta [bold green][COOKED][/bold green]")
+            rich.print("A tavola!")
 
 # If necessary, generates the necessary files for the VerifIt package: verifit_golden.py and config.ver
 def verifit_setup():
@@ -114,10 +161,10 @@ def verifit_setup():
         rich.print("[orange]WARNING: 'verifit_golden.py' already exists in the current directory.[\orange]")    
     else: 
         run_util._copy_package_file("templates/verifit_golden.py")
-        rich.print("Generation of 'verifit_golden.py' [bold green][✔][/bold green]")
+        rich.print("Generation of 'verifit_golden.py' [bold green][OK][/bold green]")
     
     if os.path.exists(f"{current_directory}/config.ver"):
         rich.print("[orange]WARNING: 'config.ver' already exists in the current directory.[\orange]")
     else:
         run_util._copy_package_file("templates/config.ver")
-        rich.print("Generation of 'config.ver' [bold green][✔][/bold green]")
+        rich.print("Generation of 'config.ver' [bold green][OK][/bold green]")
