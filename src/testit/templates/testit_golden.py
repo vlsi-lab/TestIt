8e3ecc4a448725f16e638e83ee55211f18d59b51
LuigiGiuffrida98 <luigi.giuffrida@polito.it>
2025-03-08 18:59:09 +0100

    Add license header

--- src/testit/templates/testit_golden.py
+++ src/testit/templates/testit_golden.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2025 Politecnico di Torino
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 # This is a template for the golden function(s) that can be used to define the expected behaviour 
 # of TestIt tests. The golden function(s) should use the NumPy library, as TestIt produces NumPy tensors
 # as input data and expects NumPy tensors as output data. 
